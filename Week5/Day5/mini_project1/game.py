3028537fd0d992d7c8a12c6802b85bd4f28e9c09
kako04 <kako.mekinulovi@gmail.com>
2023-03-26 00:27:29 +0300

    w5d5mini1

--- Week5/Day5/mini_project1/game.py
+++ Week5/Day5/mini_project1/game.py
@@ -20,11 +20,11 @@ class Game:
         elif (user_item == "rock" and computer_item == "scissors") or (user_item == "paper" and computer_item == "rock") or (user_item == "scissors" and computer_item == "paper"):
             return "win"
         else:
-            return "lose"
+            return "loss"
 
     def play(self):
         user_item = self.get_user_item()
         computer_item = self.get_computer_item()
         result = self.get_game_result(user_item, computer_item)
         print(f"You selected {user_item}. The computer selected {computer_item}. You {result}!")
-        return f"{result};"
+        return result
--- Week5/Day5/mini_project1/rock-paper-scissors.py
+++ Week5/Day5/mini_project1/rock-paper-scissors.py
@@ -1,4 +1,32 @@
 from game import Game
 
-game = Game()
-result = game.play()
+def get_user_menu_choice():
+    while True:
+        choice = input("Please select an option:\n1. Play a new game\n2. Show scores\n3. Quit\n")
+        if choice in ['1', '2', '3']:
+            return choice
+        else:
+            print("Invalid input. Please enter a number between 1 and 3.")
+            
+def print_results(results):
+    print("Thank you for playing! Here are the results:")
+    print(f"Wins: {results['win']}")
+    print(f"Losses: {results['loss']}")
+    print(f"Draws: {results['draw']}")
+    
+def main():
+    results = {'win': 0, 'loss': 0, 'draw': 0}
+    while True:
+        choice = get_user_menu_choice()
+        if choice == '1':
+            game = Game()
+            result = game.play()
+            results[result] += 1
+        elif choice == '2':
+            print_results(results)
+        else:
+            print_results(results)
+            break
+
+if __name__ == '__main__':
+    main()
