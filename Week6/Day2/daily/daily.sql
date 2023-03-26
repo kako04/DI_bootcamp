d3ef7eff8040b46588b421c9c354a181e6ce33df
kako04 <kako.mekinulovi@gmail.com>
2023-03-26 14:14:01 +0300

    w6d2daily

---
+++ Week6/Day2/daily/daily.sql
@@ -0,0 +1,30 @@
+-- CREATE TABLE FirstTab (
+--      id integer, 
+--      name VARCHAR(10)
+-- )
+
+-- INSERT INTO FirstTab VALUES
+-- (5,'Pawan'),
+-- (6,'Sharlee'),
+-- (7,'Krish'),
+-- (NULL,'Avtaar')
+
+-- SELECT * FROM FirstTab
+
+-- CREATE TABLE SecondTab (
+--     id integer 
+-- )
+
+-- INSERT INTO SecondTab VALUES
+-- (5),
+-- (NULL)
+
+
+-- SELECT * FROM SecondTab
+
+-- Q1 count 0
+
+-- Q2 count 6
+-- q3 0
+
+-- q4 6
