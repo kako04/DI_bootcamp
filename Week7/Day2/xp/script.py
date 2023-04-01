bc17bf6fe81d12ba289aa3a6dfd6299e31cf57b9
kako04 <kako.mekinulovi@gmail.com>
2023-04-01 20:51:49 +0300

    7_1xp

---
+++ Week7/Day2/xp/script.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+url = "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
+df = pd.read_csv(url)
+
+print(df.describe())
+
+
+sns.distplot(df["Age"].dropna(), kde=False)
+plt.show()
+
+
+sns.scatterplot(x="Age", y="Fare", data=df)
+plt.show()
+
+
+df["Age"].fillna(df["Age"].median(), inplace=True)
+
+
+df = pd.get_dummies(df, columns=["Sex", "Embarked"])
+
+
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+df[["Age", "Fare"]] = scaler.fit_transform(df[["Age", "Fare"]])
+
+
+sns.boxplot(x="Pclass", y="Age", data=df)
+plt.show()
+
+
+df.drop(columns=["PassengerId", "Name", "Ticket", "Cabin"], inplace=True)
+
+
+from sklearn.model_selection import train_test_split
+
+X = df.drop(columns=["Survived"])
+y = df["Survived"]
+X_train, X_test, y_train
